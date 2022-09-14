6da57114c42ae82ea1204d201b1a47edf502c9a6
angeldesweb <angeldesweb@gmail.com>
2022-09-14 15:53:52 -0400

    feature:some states added

---
+++ src/App.svelte
@@ -0,0 +1,5 @@
+<script>
+  import Button from "./components/Actions/Button.svelte";
+
+</script>
+
---
+++ src/components/Actions/Button.svelte
@@ -0,0 +1,30 @@
+<script>
+    export let primary = false;
+    export let secondary = false;
+    export let accent = false;
+    export let ghost = false;
+    export let link = false;
+    export let active = false;
+    export let info = false;
+    export let success = false;
+    export let warning = false;
+    export let error = false;
+    export let outline = false;
+</script>
+
+<button
+    class="btn"
+    class:btn-primary={primary}
+    class:btn-secondary={secondary}
+    class:btn-accent={accent}
+    class:btn-ghost={ghost}
+    class:btn-link={link}
+    class:btn-active={active}
+    class:btn-info={info}
+    class:btn-success={success}
+    class:btn-warning={warning}
+    class:btn-error={error}
+    class:btn-outline={outline}
+>
+button
+</button>
